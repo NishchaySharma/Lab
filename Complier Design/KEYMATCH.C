09656570d70f3b9101fe5d1332a51e3a669e4d35
Nishchay Sharma <42491880+NishchaySharma@users.noreply.github.com>
2019-02-15 00:39:12 +0530

    Update KEYMATCH.C

--- Complier Design/KEYMATCH.C
+++ Complier Design/KEYMATCH.C
@@ -16,11 +16,10 @@ void main(){
 			token=strtok(NULL,delimit);
 		}
 	}
+	fclose(fread);
 	getch();
 }
 int checkKey(char ch[]){
-
-	int size=32,i;
 	if(!strcmp(ch,"auto") || !strcmp(ch,"break") || !strcmp(ch,"case") || !strcmp(ch,"char") || !strcmp(ch,"const") || !strcmp(ch,"continue") || !strcmp(ch,"default") || !strcmp(ch,"do") || !strcmp(ch,"double") || !strcmp(ch,"else") || !strcmp(ch,"enum") || !strcmp(ch,"extern") || !strcmp(ch,"float") || !strcmp(ch,"for") || !strcmp(ch,"goto") || !strcmp(ch,"if") || !strcmp(ch,"int") || !strcmp(ch,"long") || !strcmp(ch,"register") || !strcmp(ch,"return") || !strcmp(ch,"struct") || !strcmp(ch,"switch") || !strcmp(ch,"typedef") || !strcmp(ch,"union") || !strcmp(ch,"unsigned") || !strcmp(ch,"void") || !strcmp(ch,"volatile") || !strcmp(ch,"while") || !strcmp(ch,"auto") || !strcmp(ch,"short") || !strcmp(ch,"signed") || !strcmp(ch,"short") || !strcmp(ch,"sizeof") || !strcmp(ch,"static"))
 			return 1;
 	return 0;
