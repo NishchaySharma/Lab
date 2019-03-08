83977e8e4c9fafd0d1adde7483d1e233ef2c536c
Nishchay Sharma <42491880+NishchaySharma@users.noreply.github.com>
2019-03-08 10:44:56 +0530

    Update Identifier.c

--- Complier Design/Identifier.c
+++ Complier Design/Identifier.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<string.h>
 int checkKey(char[]);
-void main(){
+int checkId(char[]);
+int main(){
 	FILE *fread;
 	char c,ch[100],*token,delimit[]=" \n\t)(";
 	fread=fopen("identify.txt","r");
@@ -16,7 +17,6 @@ void main(){
 		}
 	}
 	fclose(fread);
-	getch();
 }
 int checkKey(char ch[]){
 	if(!strcmp(ch,"auto") || !strcmp(ch,"break") || !strcmp(ch,"case") || !strcmp(ch,"char") || !strcmp(ch,"const") || !strcmp(ch,"continue") || !strcmp(ch,"default") || !strcmp(ch,"do") || !strcmp(ch,"double") || !strcmp(ch,"else") || !strcmp(ch,"enum") || !strcmp(ch,"extern") || !strcmp(ch,"float") || !strcmp(ch,"for") || !strcmp(ch,"goto") || !strcmp(ch,"if") || !strcmp(ch,"int") || !strcmp(ch,"long") || !strcmp(ch,"register") || !strcmp(ch,"return") || !strcmp(ch,"struct") || !strcmp(ch,"switch") || !strcmp(ch,"typedef") || !strcmp(ch,"union") || !strcmp(ch,"unsigned") || !strcmp(ch,"void") || !strcmp(ch,"volatile") || !strcmp(ch,"while") || !strcmp(ch,"auto") || !strcmp(ch,"short") || !strcmp(ch,"signed") || !strcmp(ch,"short") || !strcmp(ch,"sizeof") || !strcmp(ch,"static"))
